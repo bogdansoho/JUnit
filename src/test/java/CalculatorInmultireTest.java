7bcc718a090de2a313d36c0dd46d27b4fa659cb1
bogdansoho <bogdansoho58@gmail.com>
2025-11-08 10:20:27 +0200

    Test de inceput cu impartire.

--- src/test/java/CalculatorInmultireTest.java
+++ src/test/java/CalculatorInmultireTest.java
@@ -3,11 +3,12 @@ import org.junit.*;
 
 public class CalculatorInmultireTest {
     @Before
-    public void before(){
+    public void before() {
         System.out.println("Test de inceput");
     }
+
     @Test
-    public void addPositiveIntegersTest() {
+    public void dividePositiveIntegersTest() {
         CalculatorInmultire calculator = new CalculatorInmultire();
         int result = calculator.divide(10, 5);
         Assert.assertEquals(2, result);
